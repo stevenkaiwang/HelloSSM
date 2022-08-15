13960fc1e389e3f0e6d10bc8789824300064deeb
steven <2635634760@qq.com>
2022-08-16 07:00:15 +0800

    add resulttype test files

--- mybatis/mybatis_demo/mybatis_resultMap/src/main/java/com/atguigu/mybatis/mappers/EmpMapper.java
+++ mybatis/mybatis_demo/mybatis_resultMap/src/main/java/com/atguigu/mybatis/mappers/EmpMapper.java
@@ -3,9 +3,20 @@ package com.atguigu.mybatis.mappers;
 import com.atguigu.mybatis.pojo.Emp;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.HashMap;
+import java.util.List;
+
 public interface EmpMapper {
     Emp getEmpByEmpId(@Param("empId") Integer empId);
 
     Emp getEmpAndDeptByEmpId(@Param("empId") Integer empId);
+
+    Emp getEmpAndDeptByEmpIdWithAssociation(@Param("empId") Integer empId);
+
+    Emp getEmpAndDeptByStep(@Param("empId") Integer empId);
+
+    Emp getEmpsOfDept(@Param("deptId") Integer deptId);
+
+    List<Emp> getEmpsByCondition(HashMap<String,Object> conditions);
 
 }
--- mybatis/mybatis_demo/mybatis_resultMap/src/test/java/ResultTypeTest.java
+++ mybatis/mybatis_demo/mybatis_resultMap/src/test/java/ResultTypeTest.java
@@ -1,8 +1,14 @@
+import com.atguigu.mybatis.mappers.DeptMapper;
 import com.atguigu.mybatis.mappers.EmpMapper;
+import com.atguigu.mybatis.pojo.Dept;
 import com.atguigu.mybatis.pojo.Emp;
 import com.atguigu.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ResultTypeTest {
 //    @Test
@@ -14,12 +20,63 @@ public class ResultTypeTest {
 //        session.close();
 //    }
 
+//    @Test
+//    public void testGetEmpAndDeptByEmpId(){
+//        SqlSession session = SqlSessionUtil.getSession();
+//        EmpMapper mapper = session.getMapper(EmpMapper.class);
+//        Emp emp = mapper.getEmpAndDeptByEmpId(1);
+//        System.out.println(emp);
+//        session.close();
+//    }
+
+//    @Test
+//    public void testGetEmpAndDeptByEmpIdWithAssociation(){
+//        SqlSession sqlSession = SqlSessionUtil.getSession();
+//        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+//        Emp emp = mapper.getEmpAndDeptByEmpIdWithAssociation(5);
+//        System.out.println(emp);
+//        sqlSession.close();
+//    }
+
+//    @Test
+//    public void testGetEmpAndDeptByStep(){
+//        SqlSession sqlSession = SqlSessionUtil.getSession();
+//        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+//        Emp emp = mapper.getEmpAndDeptByStep(3);
+//        System.out.println(emp);
+//        sqlSession.close();
+//    }
+
+//    @Test
+//    public void getDeptEmpByDid(){
+//        SqlSession sqlSession = SqlSessionUtil.getSession();
+//        DeptMapper mapper = sqlSession.getMapper(DeptMapper.class);
+//        Dept dept = mapper.getDeptEmpByDid(1);
+//        System.out.println(dept);
+//        sqlSession.close();
+//    }
+
     @Test
-    public void testgetEmpAndDeptByEmpId(){
-        SqlSession session = SqlSessionUtil.getSession();
-        EmpMapper mapper = session.getMapper(EmpMapper.class);
-        Emp emp = mapper.getEmpAndDeptByEmpId(1);
-        System.out.println(emp);
-        session.close();
+    public void getDetpByStep(){
+        SqlSession sqlSession = SqlSessionUtil.getSession();
+        DeptMapper mapper = sqlSession.getMapper(DeptMapper.class);
+        Dept dept = mapper.getDetpByStep(2);
+        System.out.println(dept);
+        sqlSession.close();
     }
+
+    @Test
+    public void TestGetEmpsByCondition(){
+        SqlSession sqlSession = SqlSessionUtil.getSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        HashMap<String, Object> condition = new HashMap<String, Object>();
+//        condition.put("empName", "");
+        condition.put("gender", "男");
+        List<Emp> emps = mapper.getEmpsByCondition(condition);
+        emps.forEach(emp -> System.out.println(emp));
+//        System.out.println(emp);
+        sqlSession.close();
+    }
+
+
 }
